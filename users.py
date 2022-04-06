6b153dbf83def3d5171159746ee201ce698cf253
ChameeraD <chameeradulanga87@gmail.com>
2022-04-06 07:21:56 +0530

    users service

--- users.py
+++ users.py
@@ -5,5 +5,6 @@ user_api = Blueprint('user_api', __name__)
 
 @user_api.route('/users')
 def get_users():
-    r =requests.get('https://jsonplaceholder.typicode.com/users')
+    r =requests.get('https://jsonplaceholder.typicode.com/uses')
+    print(r)
     return r.text
